ddba62840303aead471f0211b1fe86d49a2ec7b1
ug <gr8.distance@gmail.com>
2024-01-19 09:42:11 +0900

    当該ページへのアクセスにも対応する

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,11 @@
 class Book < ApplicationRecord
+  def to_url
+    to_oreily.to_url
+  end
+
+  private
+
+  def to_oreily
+    Oreily::Book.new(self)
+  end
 end
--- lib/oreily/book.rb
+++ lib/oreily/book.rb
@@ -6,10 +6,14 @@ module Oreily
   class Book
     attr_reader :isbn, :title, :price, :published_on
     def initialize(record)
-      @isbn = record[0]
-      @title = record[1]
-      @price = record[2].gsub(',', '').to_i
-      @published_on = parse_date(record[3])
+      case record
+      when Array
+        intialize_from_array(record)
+      when ::Book
+        initialize_from_model(record)
+      else
+        raise ArgumentError, "Invalid argument class: #{record.class}"
+      end
     end
 
     def to_url
@@ -26,6 +30,24 @@ module Oreily
     end
 
     private
+
+    def intialize_from_array(array)
+      @isbn = record[0]
+      @title = record[1]
+      @price = record[2].gsub(',', '').to_i
+      @published_on = parse_date(record[3])
+    end
+
+    def initialize_from_model(model)
+      @isbn = model.isbn
+      @title = model.title
+      @price = model.price
+      @published_on = model.published_on
+    end
+
+    def set_price
+      record[2].gsub(',', '').to_i
+    end
 
     def parse_date(date)
       return nil if date.nil?
